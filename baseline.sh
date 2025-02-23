168858fa5d73aa7b35445ded0fd6990585c8a228
jonasbn <jonasbn@gmail.com>
2025-02-23 17:08:24 +0100

    Initial revision

---
+++ baseline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# iteratate over a list of directories and descend into each one
+# and pull the latest changes from the remote repository
+for dir in $(ls -d */); do
+  echo "Directory: $dir"
+  cd "$dir" || exit 1
+  echo "Pulling latest changes from remote repository"
+  git pull
+  cd ..
+done
